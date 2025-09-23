6b540cd3e1f486696de1d17d0804f16d167a5bc5
Subhamstar <mr.subhamdas999@gmail.com>
2025-09-23 15:18:24 +0530

    Multithreading in java

---
+++ abc.java
@@ -0,0 +1,14 @@
+public class abc {
+    static class MyThread extends Thread {
+        public void run() {
+            System.out.println("Running in: " + Thread.currentThread().getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        t1.start();
+        t2.start();
+    }
+}
