1357b5471460bdd1cc44f011b4d7c0d79e325708
Subhamstar <mr.subhamdas999@gmail.com>
2025-09-24 22:07:06 +0530

    updated

--- xyz.java
+++ xyz.java
@@ -3,21 +3,21 @@
 public class xyz {
     static class Animal {
         void sound() {
-            System.out.println("Animal makes a sound");
+            System.out.println("Animal Can eat !! ");
         }
     }
 
     static class Dog extends Animal {
         @Override
         void sound() {
-            System.out.println("Dog barks");
+            System.out.println("Dog eat");
         }
     }
 
     static class Cat extends Animal {
         @Override
         void sound() {
-            System.out.println("Cat meows");
+            System.out.println("Cat eat");
         }
     }
     public static void main(String[] args) {
