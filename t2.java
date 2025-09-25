b1235578db93f45f9cd6f429c92a997153757981
Subhamstar <mr.subhamdas999@gmail.com>
2025-09-25 23:08:59 +0530

    threading question 2

---
+++ t2.java
@@ -0,0 +1,22 @@
+public class t2 {
+    static class MyThread extends Thread {
+    private static String name;
+     MyThread(String name) { this.name = name; }
+
+    public void run() {
+        for(int i=1; i<=3; i++) {
+            System.out.println(name + " running: " + i);
+            try { Thread.sleep(500); } catch(Exception e) {}
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread("Thread-1");
+        MyThread t2 = new MyThread("Thread-2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
+}
