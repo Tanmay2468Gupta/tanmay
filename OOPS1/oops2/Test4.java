8efcd8e5d19837a0b0ba97dffd1671c268abe6c3
Subhamstar <mr.subhamdas999@gmail.com>
2025-10-09 23:39:08 +0530

    using static

---
+++ OOPS1/oops2/Test4.java
@@ -0,0 +1,16 @@
+package OOPS1.oops2;
+
+class A {
+    static void show() { System.out.println("Static A"); }
+}
+
+class B extends A {
+    static void show() { System.out.println("Static B"); }
+}
+
+public class Test4 {
+    public static void main(String[] args) {
+        A obj = new B();
+        obj.show();
+    }
+}
