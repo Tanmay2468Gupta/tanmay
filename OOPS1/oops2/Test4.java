68d87269e49c9cd88543f8a0f7cff3be12395bc9
Subhamstar <mr.subhamdas999@gmail.com>
2025-10-09 23:40:08 +0530

    static method

--- OOPS1/oops2/Test4.java
+++ OOPS1/oops2/Test4.java
@@ -8,7 +8,7 @@ class B extends A {
     static void show() { System.out.println("Static B"); }
 }
 
-public class Test4 {  // static not overridden 
+public class Test4 {  // Static methods are not overridden, they are hidden.
     public static void main(String[] args) {
         A obj = new B();
         obj.show();
