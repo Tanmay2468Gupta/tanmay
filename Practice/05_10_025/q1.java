007a2fdef6a76c458dc44e483b4daa78c16c2264
Subhamstar <mr.subhamdas999@gmail.com>
2025-10-10 23:42:21 +0530

    Updated ++

--- Practice/05_10_025/q1.java
+++ Practice/05_10_025/q1.java
@@ -3,15 +3,23 @@ public class q1 {
         A() {
             System.out.println("A constructor");
         }
+        static void show(){
+            System.out.println("A show !!");
+        }
     }
 
     static class B extends A {
         B() {
             System.out.println("B constructor");
         }
+        static void show(){
+            System.out.println("B show !!");
+        }
     }
 
     public static void main(String[] args) {
-        B obj = new B();
+        A obj = new B();
+        // B obj =new B();
+        obj.show();
     }
 }
