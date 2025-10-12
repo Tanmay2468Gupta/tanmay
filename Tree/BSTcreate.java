c7c90e313a97ec732a3282e0b46955493085ee5a
Subhamstar <mr.subhamdas999@gmail.com>
2025-10-12 22:30:27 +0530

    done

--- Tree/BSTcreate.java
+++ Tree/BSTcreate.java
@@ -9,16 +9,14 @@ class TreeNode{
         this.right=null;
     }
 }
-
-
 public class BSTcreate {
     public static TreeNode create(int arr[],int left,int right){
-            if(left>right)return null;
-            int mid=left+(right-left)/2;
-            TreeNode head=new TreeNode(arr[mid]);
-            head.left=create(arr,left,mid-1);
-            head.right=create(arr,mid+1,right);
-            return head;
+        if(left>right)return null;
+        int mid=left+(right-left)/2;
+        TreeNode head=new TreeNode(arr[mid]);
+        head.left=create(arr,left,mid-1);
+        head.right=create(arr,mid+1,right);
+        return head;
     }
     public static TreeNode createBst(int arr[]){
         Arrays.sort(arr);
