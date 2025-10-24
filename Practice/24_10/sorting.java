7e23a02409122d446ecdafa4d4f71774d32b7000
Subhamstar <mr.subhamdas999@gmail.com>
2025-10-24 23:55:07 +0530

    bubblesort practice

---
+++ Practice/24_10/sorting.java
@@ -0,0 +1,23 @@
+// package Practice.24_10;
+
+public class sorting {
+    public static void bubblesort(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<=n-2;i++){
+            int mini=i;
+            for(int j=i;j<=n-1;j++){
+                if(arr[j]<arr[mini])mini=j;
+            }
+            int temp=arr[i];
+            arr[i]=arr[mini];
+            arr[mini]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={13,46,24,52,20,9};
+        bubblesort(arr);
+        for(int i=0;i<6;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
