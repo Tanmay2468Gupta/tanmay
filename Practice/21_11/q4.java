da1015d434452be02a6a6a73710125e1e933a62b
Subhamstar <mr.subhamdas999@gmail.com>
2025-11-22 23:28:41 +0530

    prac

--- Practice/21_11/q4.java
+++ Practice/21_11/q4.java
@@ -7,6 +7,7 @@ public class q4 {
     public static void main(String[] args) {
         List<Integer>list=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         List<Integer>even=list.stream().filter(e->e%2==0).collect(Collectors.toList());
+        
         List<Integer>odd=list.stream().filter(e->e%2!=0).collect(Collectors.toList());
         System.out.println(even);
         System.out.println(odd);
