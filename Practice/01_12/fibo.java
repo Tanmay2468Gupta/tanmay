48a65120ca1ec4e6c4b058345833716d1a68b901
Subhamstar <mr.subhamdas999@gmail.com>
2025-12-02 23:29:00 +0530

    space optimized

--- Practice/01_12/fibo.java
+++ Practice/01_12/fibo.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class fibo {
     public static int fibonacchi(int n,Integer dp[]){
         if(n==0)return 0;
@@ -6,7 +7,19 @@ public class fibo {
         return dp[n]=fibonacchi(n-1,dp)+fibonacchi(n-2,dp);
     }
     public static void main(String[] args) {
-        Integer dp[]=new Integer[6];
-        System.out.println(fibonacchi(5,dp));
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Integer dp[]=new Integer[n+1];
+        System.out.println(fibonacchi(n,dp));
+
+
+        int prev2=0;
+        int prev=1;
+        for(int i=2;i<=n;i++){
+            int curr=prev2+prev;
+            prev2=prev;
+            prev=curr;
+        }
+        System.out.println(prev);
     }
 }
