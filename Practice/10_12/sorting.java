7a8f7a81c5b559d6fe967db627cd53e21be90517
Subhamstar <mr.subhamdas999@gmail.com>
2025-12-10 16:35:57 +0530

    buuble sort

--- Practice/10_12/sorting.java
+++ Practice/10_12/sorting.java
@@ -20,9 +20,23 @@ public class sorting {
             arr[mini]=temp;
         }
     }
+    public static void bubbleSort(int arr[]){
+        int n=arr.length;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    
     public static void main(String[] args) {
         int arr[]={13,46,24,52,20,9};
-        selectionSort(arr);
+        // selectionSort(arr);
+        // bubbleSort(arr);
         print(arr);
     }
 }
