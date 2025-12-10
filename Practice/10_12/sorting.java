8504983eac58353ae24b0c22978cbafcb5302c75
Subhamstar <mr.subhamdas999@gmail.com>
2025-12-10 17:04:06 +0530

    insertion Sort

--- Practice/10_12/sorting.java
+++ Practice/10_12/sorting.java
@@ -32,11 +32,23 @@ public class sorting {
             }
         }
     }
-    
+    public static void insertion(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<=n-1;i++){
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                int temp=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]=temp;
+                j--;
+            }
+        }
+    }
     public static void main(String[] args) {
         int arr[]={13,46,24,52,20,9};
         // selectionSort(arr);
         // bubbleSort(arr);
+        insertion(arr);
         print(arr);
     }
 }
